c76b5a771981149c17e5f6aa8a284073ad4d0fae
Nikolas Tezak <ntezak@stanford.edu>
2014-01-13 13:11:22 -0800

    made cysolve proper package

---
+++ cysolve/__init__.py
@@ -0,0 +1,8 @@
+
+"""
+The cysolve module comes with the following submodules:
+
+1) cysolve.ode:
+    This provides mechanisms to efficiently solve (in parallel) a given ode for many parametersets.
+
+"""
